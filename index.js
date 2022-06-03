76540eaee02d0adec7bb7b7fd2ab23b206a050c2
trunkey2003 <21521049@gm.uit.edu.vn>
2022-06-03 21:04:12 +0700

    update

--- index.js
+++ index.js
@@ -1,62 +1,49 @@
 const fs = require("fs");
+const findString = "d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d3f4x2em mdeji52x a5q79mjw g1cxx5fr lrazzd5p oo9gr5id";
+
+const findName = (data, index) => {
+    index = index + findString.length;
+    while (data[index] != '>') {
+        index++;
+    };
+    index++;
+    let endOfName = index;
+    while (data[endOfName] != '<') {
+        endOfName++;
+    };
+    let name = data.substring(index, endOfName);
+    name = name.replace(/\s{2,}/g, ' ').trim();
+    name = name.replace('\n', ``);
+    return name;
+};
 
 // __dirname means relative to script. Use "./data.txt" if you want it relative to execution path.
 fs.readFile(__dirname + "/input.html", (error, data) => {
-    if(error) {
+    if (error) {
         throw error;
     };
 
     data = data.toString();
-    const findString = "d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d3f4x2em mdeji52x a5q79mjw g1cxx5fr lrazzd5p oo9gr5id";
-    var count = 0;
-    var friends = [];
+    
+    const friends = [];
+    const matchedStrings = [...data.matchAll(findString)];
 
-    for(var i = 0; i < data.length; i++) {
-        if (data[i] === findString[0]) {
-            var iSave = i;
-            var iLink = i + 1;
-            var isFindString = true;
-            for(let j = 1; j < findString.length;j++){
-                if (!(data[iLink] == findString[j])){
-                    isFindString = false;
-                    break;
-                };
-                iLink++;
-            };
-
-            if (!isFindString){
-                // i = i + findString.length;
-            } else {
-                count++;
-                i = i + findString.length;
-                while(data[i] != '>'){
-                    i++;
-                };
-                i++;
-                let endOfName = i;
-                while (data[endOfName] != '<'){
-                    endOfName++;
-                };
-                let name = data.substring(i, endOfName);
-                name = name.replace(/\s{2,}/g, ' ').trim();
-                name = name.replace('\n',``);
-                // console.log("Name = ", name);
-                friends.push(name);
-            };
-        };
-    };
+    matchedStrings.forEach(({ index }) => {
+        const name = findName(data,index);
+        friends.push(name);
+    });
 
     friends.sort();
-    
+
     const output = friends.join('\n');
 
     fs.writeFile(__dirname + "/output.txt", output, err => {
         if (err) {
-          console.error(err)
-          return
-        }
-        //file written successfully
+            console.error(err)
+            return
+        };
+        console.log("File has been created");
     });
-    console.log("Total friends: ", count);
+    console.log("Total friends: ", friends.length);
 });
 
